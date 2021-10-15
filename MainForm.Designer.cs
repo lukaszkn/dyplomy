d87545a452ff1cf0df8822abf0209cfd1b6d14ba
lukasz <lukaszkn@go2.pl>
2021-10-15 17:58:49 +0200

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Dyplomy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTemplate = new System.Windows.Forms.Label();
+            this.buttonTemplateSelection = new System.Windows.Forms.Button();
+            this.labelDataSource = new System.Windows.Forms.Label();
+            this.buttonDataSource = new System.Windows.Forms.Button();
+            this.checkBoxUniqueNames = new System.Windows.Forms.CheckBox();
+            this.buttonCreateDyplomy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTemplate
+            // 
+            this.labelTemplate.AutoSize = true;
+            this.labelTemplate.Location = new System.Drawing.Point(190, 40);
+            this.labelTemplate.Name = "labelTemplate";
+            this.labelTemplate.Size = new System.Drawing.Size(101, 25);
+            this.labelTemplate.TabIndex = 0;
+            this.labelTemplate.Text = "Template";
+            // 
+            // buttonTemplateSelection
+            // 
+            this.buttonTemplateSelection.Location = new System.Drawing.Point(12, 23);
+            this.buttonTemplateSelection.Name = "buttonTemplateSelection";
+            this.buttonTemplateSelection.Size = new System.Drawing.Size(150, 58);
+            this.buttonTemplateSelection.TabIndex = 1;
+            this.buttonTemplateSelection.Text = "Browse";
+            this.buttonTemplateSelection.UseVisualStyleBackColor = true;
+            this.buttonTemplateSelection.Click += new System.EventHandler(this.buttonTemplateSelection_Click);
+            // 
+            // labelDataSource
+            // 
+            this.labelDataSource.AutoSize = true;
+            this.labelDataSource.Location = new System.Drawing.Point(190, 116);
+            this.labelDataSource.Name = "labelDataSource";
+            this.labelDataSource.Size = new System.Drawing.Size(128, 25);
+            this.labelDataSource.TabIndex = 2;
+            this.labelDataSource.Text = "Data source";
+            // 
+            // buttonDataSource
+            // 
+            this.buttonDataSource.Location = new System.Drawing.Point(12, 99);
+            this.buttonDataSource.Name = "buttonDataSource";
+            this.buttonDataSource.Size = new System.Drawing.Size(150, 58);
+            this.buttonDataSource.TabIndex = 3;
+            this.buttonDataSource.Text = "Browse";
+            this.buttonDataSource.UseVisualStyleBackColor = true;
+            this.buttonDataSource.Click += new System.EventHandler(this.buttonDataSource_Click);
+            // 
+            // checkBoxUniqueNames
+            // 
+            this.checkBoxUniqueNames.AutoSize = true;
+            this.checkBoxUniqueNames.Location = new System.Drawing.Point(12, 186);
+            this.checkBoxUniqueNames.Name = "checkBoxUniqueNames";
+            this.checkBoxUniqueNames.Size = new System.Drawing.Size(228, 29);
+            this.checkBoxUniqueNames.TabIndex = 4;
+            this.checkBoxUniqueNames.Text = "Unique names only";
+            this.checkBoxUniqueNames.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreateDyplomy
+            // 
+            this.buttonCreateDyplomy.Location = new System.Drawing.Point(412, 267);
+            this.buttonCreateDyplomy.Name = "buttonCreateDyplomy";
+            this.buttonCreateDyplomy.Size = new System.Drawing.Size(345, 102);
+            this.buttonCreateDyplomy.TabIndex = 5;
+            this.buttonCreateDyplomy.Text = "Create dyplomy";
+            this.buttonCreateDyplomy.UseVisualStyleBackColor = true;
+            this.buttonCreateDyplomy.Click += new System.EventHandler(this.buttonCreateDyplomy_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 424);
+            this.Controls.Add(this.buttonCreateDyplomy);
+            this.Controls.Add(this.checkBoxUniqueNames);
+            this.Controls.Add(this.buttonDataSource);
+            this.Controls.Add(this.labelDataSource);
+            this.Controls.Add(this.buttonTemplateSelection);
+            this.Controls.Add(this.labelTemplate);
+            this.Name = "MainForm";
+            this.Text = "Dyplomy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTemplate;
+        private System.Windows.Forms.Button buttonTemplateSelection;
+        private System.Windows.Forms.Label labelDataSource;
+        private System.Windows.Forms.Button buttonDataSource;
+        private System.Windows.Forms.CheckBox checkBoxUniqueNames;
+        private System.Windows.Forms.Button buttonCreateDyplomy;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,120 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Dyplomy
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTemplateSelection_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DOCX Files|*.docx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelTemplate.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonDataSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files|*.csv";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelDataSource.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonCreateDyplomy_Click(object sender, EventArgs e)
+        {
+            List<string> uniqueNames = new List<string>();
+
+            int dyplomyCount = 0;
+            FileInfo info = new FileInfo(labelTemplate.Text);
+
+            string directory = info.Directory + "\\" + info.Name.Replace(".docx", "");
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            FastZip fastZip = new FastZip();
+
+            string[] nazwy = File.ReadAllLines(labelDataSource.Text);
+
+            foreach (var line in nazwy)
+            {
+                if (line.Contains("nazwa"))
+                    continue;
+
+                string linia = line;
+                if (line.Contains("\","))
+                    linia = linia.Replace("\",", ";").Replace("\"", "");
+                else
+                    linia = linia.Replace(",", ";");
+
+                string[] split = linia.Split(new char[] { ';' });
+                if (split[0].Trim().Length == 0) continue;
+                if (split.Length > 1 && split[1].Trim().Contains("-")) continue;
+
+                if (checkBoxUniqueNames.Checked && uniqueNames.Contains(split[0])) continue;
+
+                string targetDir = directory + "\\" + split[0] + " " + info.Name + "___";
+                fastZip.ExtractZip(labelTemplate.Text, targetDir, null);
+
+                ReplaceTextInFolder(targetDir, "[nazwa]", split[0]);
+                if (split.Length > 1)
+                {
+                    ReplaceTextInFolder(targetDir, "[miejsce]", split[1]);
+                }
+
+                fastZip.CreateZip(targetDir.Replace("___", ""), targetDir, true, null);
+
+                uniqueNames.Add(split[0]);
+
+                dyplomyCount++;
+
+                try
+                {
+                    Thread.Sleep(100);
+                    Directory.Delete(targetDir, true);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            MessageBox.Show(dyplomyCount + " dyplomy created");
+        }
+
+        private void ReplaceTextInFolder(string targetDir, string textToFind, string replaceWith)
+        {
+            string[] entries = Directory.GetFileSystemEntries(targetDir, "*.xml", SearchOption.AllDirectories);
+
+            foreach (var file in entries)
+            {
+                string text = File.ReadAllText(file);
+
+                if (text.Contains(textToFind))
+                {
+                    text = text.Replace(textToFind, replaceWith);
+                    File.WriteAllText(file, text, Encoding.UTF8);
+                }
+            }
+        }
+    }
+}
